ac10cfd42738b47d18e76387fae49fa6bf2f5a92
Artem Neij <artemneij@gmail.com>
2025-08-09 00:17:27 +0300

    Change2

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -9,6 +9,7 @@ function Description({defaultUserPhoto, userProfile}: UserProfileType) {
         <img className="photo--default" src={actualProtoUrl} alt="Avatar"/>
         <h4>{userProfile.userName}</h4>
         <p>{userProfile.userDescription}</p>
+        
         <p>{userProfile.openForWork ? "В поиске работы" : "Работаю на трех работах"}</p>
     </div>
     )
